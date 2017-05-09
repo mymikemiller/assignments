63f146d7b4867244d40533f0267de38e95ccfc76
Mike Miller <mikem.exe@gmail.com>
2017-05-09 11:54:06 -0600

    added to gitignore file

--- projects/penguins-vs-communists/main.js
+++ projects/penguins-vs-communists/main.js
@@ -8,7 +8,7 @@ function sendNuke(party, onHit, onMiss) {
     if (hit) {
         onHit(party);
     } else {
-
+        onMiss(party);
     }
 }
 
@@ -25,6 +25,10 @@ function miss(missedParty) {
 var penguins = new Party("Penguins");
 var communists = new Party("Communists");
 
-var penguinsTurn = Math.random() < 0.5 ? true : false;
+// Randomly select who attacks first
+var attackingParty = Math.random() < 0.5 ? true : false;
 
-sendNuke()
+while (attackedParty.population > 0 && attackingParty.population > 0) {
+    var attackedParty = attackingParty = penguins ? communists : penguins;
+    sendNuke(attackedParty, hit, miss);
+}
