564c9e5dd87936705f33745ecdc8d471cd99b905
Mike Miller <mikem.exe@gmail.com>
2017-05-27 10:13:53 -0600

    Temperature warmup almost working. The server does not have CORS enabled, so another solution (like JSONP) will be tried

--- exercises/convert-temperatures-filter/app.js
+++ exercises/convert-temperatures-filter/app.js
@@ -1,7 +1,13 @@
 var app = angular.module("convertTemperaturesApp", []);
 
-app.controller("ConvertTemperaturesCtrl", ["$scope", function ($scope) {
+app.controller("ConvertTemperaturesCtrl", ["$scope", "currentForcastService", function ($scope, currentForcastService) {
     $scope.num = 100;
+
+    currentForcastService.getCurrentWeather().then(function (response) {
+        console.log(response);
+        $scope.slcTemp = 500;
+    })
+
 }]);
 
 app.filter("temperatureConverter", function () {
@@ -16,29 +22,19 @@ app.filter("temperatureConverter", function () {
     }
 });
 
-app.service("currentForcastService", ["http", function ($http) {
+app.service("currentForcastService", ["$http", function ($http) {
     this.getCurrentWeather = function () {
-        return $http.get("https://api.darksky.net/forecast/d2f8c4384f85d6810919ec1bf05530db/40.7608,111.8910").then(
-            function (response) {
-                // success
-                return response;
-            })
-    }
-}]);
-
-
-app.service("TwitchService", ["$http", function ($http) {
-    this.searchStreams = function (searchTerm) {
-        return $http.get("https://api.twitch.tv/kraken/search/streams?query='" + searchTerm + "'", {
-            headers: {
-                'Client-ID': 'x47okantt9lixqh8iowlt0jc5kc2cv'
-            }
-        }).then(function (response) { // Success
-            console.log(response);
-            return response;
-        }, function (response) { // Error
-            console.log(response);
-            return response;
-        })
+        return $http.get("https://api.darksky.net/forecast/d2f8c4384f85d6810919ec1bf05530db/40.7608,111.8910")
+            .then(
+                function (response) {
+                    // success
+                    console.log("success making request")
+                    return response;
+                },
+                function (response) {
+                    //failure
+                    console.log("failure making request")
+                    return response;
+                })
     }
 }]);
