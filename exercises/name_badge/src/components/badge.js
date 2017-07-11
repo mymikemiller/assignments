a737542280ef40d9a0311ffedb952d0f518ac9eb
Mike Miller <mikem.exe@gmail.com>
2017-07-11 09:04:02 -0600

    Badge working, not styled

--- exercises/name_badge/src/components/badge.js
+++ exercises/name_badge/src/components/badge.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Grid, Row, Col } from "react-bootstrap"
 
 class Badge extends React.Component {
   render() {
     return (
       <div>
-        Name: {this.props.firstName} {this.props.lastName}
-        Email: {this.props.email}
-        Phone: {this.props.phone}
-        Place of Birth: {this.props.placeOfBirth}
-        Favorite Food: {this.props.favoriteFood}
+        <Grid>
+          <Row>
+            <Col xs={5}>
+              Name: {this.props.firstName} {this.props.lastName}
+            </Col>
+            <Col xs={5}>
+              Email: {this.props.email}
+            </Col>
+            Phone: {this.props.phone}
+            Place of Birth: {this.props.placeOfBirth}
+            Favorite Food: {this.props.favoriteFood}
+          </Row>
+        </Grid>
       </div>
     )
   }
