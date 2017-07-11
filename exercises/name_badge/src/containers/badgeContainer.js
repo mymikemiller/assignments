d5e89144baab3d907816f4a446e1a492ba22f329
Mike Miller <mikem.exe@gmail.com>
2017-07-11 11:16:32 -0600

    Moved state into badgeContainer

--- exercises/name_badge/src/containers/badgeContainer.js
+++ exercises/name_badge/src/containers/badgeContainer.js
@@ -1,11 +1,61 @@
 import React from "react"
 import { Button, Form, FormGroup, FormControl } from "react-bootstrap";
+import autobind from "react-autobind";
 
 import Badge from "../components/badge.js";
 
 class BadgeContainer extends React.Component {
+  componentWillMount() {
+    autobind(this);
+  }
+  constructor() {
+    super();
+    this.state = {
+      badges: [{
+        firstName: "Mike",
+        lastName: "Miller",
+        email: "mikem.exe@gmail.com",
+        placeOfBirth: "Wisconsin",
+        phone: "707.867.5350",
+        favoriteFood: "Chicken Strips",
+        about: "A software engineer with a passion for learning"
+      }],
+      firstNameValue: "",
+      lastNameValue: "",
+      emailValue: "",
+      placeOfBirthValue: "",
+      phoneValue: "",
+      favoriteFoodValue: "",
+      aboutValue: ""
+    }
+  }
+  inputFirstName(event) {
+    this.setState({
+      ...this.state,
+      firstNameValue: event.target.value
+    });
+  }
+  submit() {
+    console.log("submit");
+    let newBadge = {
+      firstName: this.state.firstNameValue,
+      lastName: this.state.lastNameValue,
+      email: this.state.email,
+      placeOfBirth: this.state.placeOfBirthValue,
+      phone: this.state.email,
+      favoriteFood: this.state.favoriteFoodValue,
+      about: this.state.aboutValue
+    }
+    let existingBadges = this.state.badges;
+    existingBadges.push(newBadge);
+    this.setState({
+      ...this.state,
+      badges: existingBadges,
+      firstNameValue: ""
+    });
+  }
   genBadges() {
-    return this.props.badges.map((badge, index) => {
+    return this.state.badges.map((badge, index) => {
       return (
         <div key={index}>
           <Badge
@@ -25,8 +75,9 @@ class BadgeContainer extends React.Component {
     return (
       <div>
         <Form onSubmit={(e) => {
-          this.props.handleSubmit();
+          console.log("onSubmit");
           e.preventDefault();
+          this.submit();
         }}>
           <FormGroup>
             <FormControl type="text" placeholder="First Name" onChange={this.props.handleInputFirstName} value={this.props.firstName} />
